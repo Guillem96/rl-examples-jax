4f6a2aed13bc0fae326f4f474cfe8b770adebb03
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2020-01-24 10:14:50 +0100

    Call the forward step with implicit parameters

--- rl_jax/nn/models.py
+++ rl_jax/nn/models.py
@@ -6,6 +6,18 @@ from ..typing import (JaxModule, JaxTensor,
                       ForwardFn, Parameter, PartialJaxModule)
 
 
+class JaxModel(JaxModule):
+    
+    def __call__(self, x: JaxTensor, vectorize: bool = False):
+        # Use this function during inference
+        if vectorize:
+          fn = jax.vmap(self.forward_fn)
+        else:
+          fn = self.forward_fn
+        
+        return fn(self.parameters, x)
+      
+
 def _sequential_forward(parameters: Sequence[Parameter], 
                         x: JaxTensor,
                         forward_fns: ForwardFn) -> JaxTensor:
@@ -16,7 +28,7 @@ def _sequential_forward(parameters: Sequence[Parameter],
     return x
 
 
-def sequential(key, *layers: Sequence[PartialJaxModule]) -> JaxModule:
+def sequential(key, *layers: Sequence[PartialJaxModule]) -> JaxModel:
     # Randomly initialize layers
     layer_keys = jax.random.split(key, num=len(layers))
     layers = [l(k) for l, k in zip(layers, layer_keys)]
